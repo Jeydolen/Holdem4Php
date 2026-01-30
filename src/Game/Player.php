55214c9a88adfca4e920f8fac6aef65e6ff68910
Adrien <contact+github@jeydolen.com>
2026-01-30 16:08:06 +0100

    Basic player implementation

--- src/Game/Player.php
+++ src/Game/Player.php
@@ -2,7 +2,10 @@
 
 namespace App\Game;
 
+use App\Game\Card\Card;
 use App\Game\CardPile\ICardPile;
+use App\Game\CardPile\PlayerHoleCards;
+use App\Game\WebSocket\ConnectionWrapper;
 
 class Player
 {
@@ -14,4 +17,30 @@ class Player
     private mixed $user;
 
     private ICardPile $hole_cards;
+
+    private ConnectionWrapper $connection;
+
+    public function __construct(string $user, ConnectionWrapper $connection)
+    {
+        // For now user, is just a string for simplicity
+        $this->user = $user;
+        $this->connection = $connection;
+        $this->hole_cards = new PlayerHoleCards(2, true);
+    }
+
+    public function isSame(Player $player): bool
+    {
+        return $this->user == $player->user;
+    }
+
+    public function getHoleCards(): ICardPile
+    {
+        return $this->hole_cards;
+    }
+
+    public function pushCard(Card $card): void
+    {
+        $this->hole_cards->pushCard($card);
+        $this->connection->sendJson(["card" => $card]);
+    }
 }
