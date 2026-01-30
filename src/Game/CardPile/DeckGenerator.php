fb7ec05996c33e5475cef8bf02ca82a196d215cc
Adrien <contact+github@jeydolen.com>
2026-01-30 16:10:16 +0100

    Deck generation service

---
+++ src/Game/CardPile/DeckGenerator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Game\CardPile;
+
+use App\DTO\DeckGenerationDTO;
+use App\Enum\DeckGenerationTypeEnum;
+use App\Game\Card\Card;
+use Exception;
+
+class DeckGenerator
+{
+    private DeckGenerationDTO $deckGenerationDTO;
+
+    public function __construct(DeckGenerationDTO $deckGenerationDTO)
+    {
+        $this->deckGenerationDTO = $deckGenerationDTO;
+    }
+
+    public function generate(): Deck
+    {
+        $deckDTO = $this->deckGenerationDTO;
+        $deck = new Deck(maxSize: $deckDTO->maxSize, noDuplicate: $deckDTO->noDuplicate);
+
+        if ($deckDTO->generationType === DeckGenerationTypeEnum::AUTOMATIC) {
+            $card_gen_config = $deckDTO->cardGenerationConfig;
+
+            if (empty($card_gen_config)) {
+                throw new Exception("Cannot generate a deck without a card generation config provided !");
+            }
+
+            foreach ($card_gen_config->ranks as $i => $rank) {
+                $weight = $i;
+                foreach ($card_gen_config->symbols as $symbol) {
+                    $card = new Card($rank, $symbol, $weight + 0);
+                    $deck->pushCard($card);
+                }
+            }
+        } else {
+            $cards = $deckDTO->cards;
+            foreach ($cards as $card) {
+                $real_card = new Card($card->getRank(), $card->getSymbol(), $card->getWeight());
+                $deck->pushCard($real_card);
+            }
+        }
+
+        return $deck;
+    }
+}
