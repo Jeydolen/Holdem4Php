ff311b81245a4b9ab7369b6e9517270cabfb0c84
Adrien <contact+github@jeydolen.com>
2026-02-01 11:36:18 +0100

    Added a way to load table rules from db when starting websocket server

--- src/Game/WebSocket/Server.php
+++ src/Game/WebSocket/Server.php
@@ -5,26 +5,36 @@ namespace App\Game\WebSocket;
 use Exception;
 use JsonException;
 
+use Workerman\Worker;
+use Workerman\Connection\TcpConnection;
+
 use App\DTO\TableRulesDTO;
+use App\DTO\DeckGenerationDTO;
+
+use App\Enum\DeckGenerationTypeEnum;
 
 use App\Game\Player;
 use App\Game\Table\Table;
+use App\Game\Hand\Phase\PhaseFactory;
+
+use App\Repository\TableRulesRepository;
+
 use Symfony\Component\Serializer\SerializerInterface;
 
-use Workerman\Worker;
-use Workerman\Connection\TcpConnection;
 
 class Server
 {
 
-    public function __construct(private SerializerInterface $serializer)
-    {
+    public function __construct(
+        private SerializerInterface $serializer,
+        private TableRulesRepository $tableRulesRepository
+    ) {
     }
 
     /** @var Table[] */
     private array $tables = [];
 
-    public function createServer(int $port)
+    public function createServer(int $port): Worker
     {
         $ws_worker = new Worker("websocket://0.0.0.0:$port");
 
@@ -39,14 +49,53 @@ class Server
         };
 
         // Emitted when connection closed
-        $ws_worker->onClose = function ($connection) {};
+        $ws_worker->onClose = function ($connection) {
+            $conn = new ConnectionWrapper($connection, $this->serializer);
+            $conn->sendJson(["connected" => false]);
+        };
 
         // Run worker
-        Worker::runAll();
+        return $ws_worker;
+    }
+
+    /**
+     * Method that create a new table for each game rules stored in db
+     * @return int Number of tables created
+     */
+    public function loadTables(): int
+    {
+        $rules = $this->tableRulesRepository->findAll();
+        foreach ($rules as $rule) {
+            // Phases sorted by priority
+            $phases = $rule->getPhases();
+
+            $game_phases = [];
+            // Game phase construction
+            foreach ($phases as $phase) {
+                $game_phase = PhaseFactory::create($phase->getType(), $phase->getTimeout(), $phase->getAdditionnalProperties());
+                $game_phases[] = $game_phase;
+            }
+
+            $deck_rules = new DeckGenerationDTO();
+            $deck_rules->cards = $rule->getCards()->toArray();
+            $deck_rules->maxSize = \sizeof($deck_rules->cards);
+            $deck_rules->noDuplicate = false;
+            $deck_rules->generationType = DeckGenerationTypeEnum::MANUAL;
+
+            $table_id = uniqid("table");
+            $this->tables[$table_id] = new Table(
+                $rule->getMaxPlayers(),
+                $game_phases,
+                $deck_rules
+            );
+        }
+
+        return \sizeof($rules);
     }
 
     private function onConnect(ConnectionWrapper $connection): void
     {
+        $connection->sendJson(["connected" => true]);
     }
 
     private function onMessage(ConnectionWrapper $connection, mixed $data): void
@@ -64,13 +113,13 @@ class Server
                 throw new Exception("No action defined !");
             }
 
-            $this->handleActions($connection, $json["action"], $json);
+            $this->handleActions($connection, $json["action"], $data);
         } catch (Exception $e) {
             $connection->sendJson(["error" => $e->getMessage()]);
         }
     }
 
-    private function handleActions(ConnectionWrapper $connection, string $action, array $data): void
+    private function handleActions(ConnectionWrapper $connection, string $action, string $data): void
     {
         if ($action === "createTable") {
             // TODO: Proper role mecanism to create new tables 
@@ -78,9 +127,18 @@ class Server
             /** @var TableRulesDTO */
             $table_rules = $this->serializer->deserialize($data, TableRulesDTO::class, "json");
 
-            return $this->createNewTable($connection, $table_rules);
+            $this->createNewTable($connection, $table_rules);
+            return;
+        }
 
-        } else if ($action === "playerJoin") {
+        // TODO: Make a real search
+        if ($action === "listTables") {
+            $connection->sendJson(["tables" => $this->tables]);
+        }
+
+        if ($action === "playerJoin") {
+            // TODO: Change for proper DTO
+            $data = json_decode($data, associative: true, flags: JSON_THROW_ON_ERROR);
             $table_id = $data["table_id"] ?? null;
             if (empty($table_id)) {
                 throw new Exception("Empty table id");
@@ -101,6 +159,8 @@ class Server
             $table->addPlayer(new Player($user_id, $connection));
             $connection->send(json_encode(["player_joined" => true]));
         } else if ($action === "startGame") {
+            // TODO: Change for proper DTO
+            $data = json_decode($data, associative: true, flags: JSON_THROW_ON_ERROR);
             $table_id = $data["table_id"] ?? null;
             if (empty($table_id)) {
                 throw new Exception("Empty table id");
