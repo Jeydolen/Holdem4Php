7e9a31a3801ebca8a8b3eaa6c98481028bbed5f5
Adrien <contact+github@jeydolen.com>
2026-02-08 20:36:23 +0100

    Added player action event

---
+++ src/Event/PlayerAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Event;
+
+use App\Game\Player;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class PlayerAction extends Event
+{
+    public function __construct(private Player $player, private mixed $data)
+    {
+    }
+
+    public function getPlayer(): Player
+    {
+        return $this->player;
+    }
+
+    public function getEventData(): mixed
+    {
+        return $this->data;
+    }
+}
