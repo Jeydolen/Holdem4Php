514ec25a8d08e40e2a97eabb4345fd2e902835bb
Adrien <contact+github@jeydolen.com>
2026-02-08 20:34:13 +0100

    Added a table registry to manage tables

---
+++ src/Game/Table/TableRegistry.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Game\Table;
+
+use Psr\Log\LoggerInterface;
+
+
+class TableRegistry
+{
+    /** @var Table[] */
+    private array $tables = [];
+
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    public function addTable(string $key, Table $table): void
+    {
+        $this->tables[$key] = $table;
+    }
+
+    public function removeTable(string $key): void
+    {
+        unset($this->tables[$key]);
+    }
+
+    public function getTable(string $key): ?Table
+    {
+        return $this->tables[$key] ?? null;
+    }
+
+    public function getAllTables(): array
+    {
+        return $this->tables;
+    }
+}
