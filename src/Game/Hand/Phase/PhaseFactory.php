cd57eac8fd55b9428d375e1c57314c932344e8bd
Adrien <contact+github@jeydolen.com>
2026-02-08 20:36:00 +0100

    Added event dispatcher for phase factory

--- src/Game/Hand/Phase/PhaseFactory.php
+++ src/Game/Hand/Phase/PhaseFactory.php
@@ -4,22 +4,28 @@ namespace App\Game\Hand\Phase;
 
 use Exception;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class PhaseFactory
 {
+    public function __construct(private LoggerInterface $logger, private EventDispatcher $dispatcher)
+    {
+    }
+
     private static array $map = [
         "draw_cards" => DrawCardPhase::class,
         "betting_phase" => BettingPhase::class,
     ];
 
-    public static function create(LoggerInterface $logger, string $type, ?int $timeout, array $data): IPhase
+    public function create(string $type, ?int $timeout, array $data): IPhase
     {
         if (empty(self::$map[$type])) {
             throw new Exception("Unknown phase");
         }
 
         // There might be a better way to do this...
-        $data["logger"] = $logger;
+        $data["logger"] = $this->logger;
+        $data["dispatch"] = $this->dispatcher;
         $data["timeout"] = $timeout;
 
         $class = self::$map[$type];
