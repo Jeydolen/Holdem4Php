c2bb8977a1a6e3a9d7bb206ceb8fc89f68079e34
Adrien <contact+github@jeydolen.com>
2026-02-08 20:30:19 +0100

    Added maxBettingAmount to betting phase

--- src/DTO/Phase/BettingPhaseDTO.php
+++ src/DTO/Phase/BettingPhaseDTO.php
@@ -2,8 +2,15 @@
 
 namespace App\DTO\Phase;
 
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
+
 class BettingPhaseDTO extends PhaseDTO
 {
+    #[NotBlank()]
+    #[Positive()]
+    public int $maxBettingAmount;
+
     public function getType(): string
     {
         return "betting_phase";
--- src/Game/Hand/Phase/BettingPhase.php
+++ src/Game/Hand/Phase/BettingPhase.php
@@ -2,14 +2,27 @@
 
 namespace App\Game\Hand\Phase;
 
+use App\Game\Player;
 use App\Game\CardPile\Deck;
+
+use App\Event\PlayerAction;
+
 use Psr\Log\LoggerInterface;
 
-class BettingPhase implements IPhase
+use Workerman\Timer;
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class BettingPhase extends AbstractPhase
 {
+    private Player $currentPlayer;
+    private int $currentMinAmount = 0;
+
     private function __construct(
+        private EventDispatcher $dispatcher,
         private LoggerInterface $logger,
-        private ?int $timeout
+        private ?int $timeout,
+        private int $maxBettingAmount
     ) {
     }
 
@@ -17,11 +30,27 @@ class BettingPhase implements IPhase
     {
         // We ask every player to bet, they can either call, check, raise or fold
         foreach ($players as $player) {
+            $this->currentPlayer = $player;
+            if (!empty($this->timeout)) {
+                Timer::add($this->timeout, function () use ($player) {
+                    $this->dispatcher->dispatch(new PlayerAction($player, ["player_action" => "fold"]));
+                });
+            }
+            $player->askBet($this->maxBettingAmount, $this->currentMinAmount);
         }
     }
 
     public static function fromArray(array $data): self
     {
-        return new self($data["logger"], $data["timeout"] ?? null);
+        return new self($data["dispatcher"], $data["logger"], $data["timeout"] ?? null, $data["maxBettingAmount"]);
+    }
+
+    public function onPlayerAction(PlayerAction $event): void
+    {
+        if ($event->getPlayer() !== $this->currentPlayer) {
+            return;
+        }
+
+
     }
 }
