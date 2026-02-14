af75e7c955cd825c7a652b195b53509726e6d853
Adrien <contact+github@jeydolen.com>
2026-02-14 18:08:45 +0100

    Added PlayerBettingAction

---
+++ src/Enum/PlayerBettingActionEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enum;
+
+enum PlayerBettingActionEnum: string
+{
+    case FOLD = "fold";
+
+    case CHECK = "check";
+
+    case BET = "bet";
+
+    case CALL = "call";
+
+    case ALL_IN = "all_in";
+}
