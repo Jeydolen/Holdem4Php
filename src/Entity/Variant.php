3997386ba508ad9435d18d38ade17d947db1c369
jeydolen <contact@jeydolen.com>
2026-07-21 14:41:27 +0100

    Format & Phpdoc

--- src/Entity/Variant.php
+++ src/Entity/Variant.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use App\Repository\VariantRepository;
+
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
+
 use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Serializer\Attribute\SerializedName;
 
@@ -158,6 +160,9 @@ class Variant
         return $this->variantPhases;
     }
 
+    /**
+     * @return Phase[]
+     */
     #[Groups("show_phase")]
     public function getPhases(): array
     {
