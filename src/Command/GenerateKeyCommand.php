778f461104c7eb0681f4c07f1dcb864812e5c511
jeydolen <contact@jeydolen.com>
2026-08-28 19:12:23 +0100

    Added path argument to generate key command

--- src/Command/GenerateKeyCommand.php
+++ src/Command/GenerateKeyCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use ParagonIE\Paseto\Keys\AsymmetricSecretKey;
 
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -14,13 +15,25 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class GenerateKeyCommand extends Command
 {
-    public function __invoke(OutputInterface $output): int
-    {
+    public function __invoke(
+        #[Argument("The path used to store keys")] ?string $path,
+        OutputInterface $output
+    ): int {
         $private_key = new AsymmetricSecretKey(sodium_crypto_sign_keypair());
         $public_key = $private_key->getPublicKey();
 
         $output->writeln("Private key:\n" . $private_key->encodePem());
         $output->writeln("Public key:\n" . $public_key->encodePem());
+
+        if (!empty($path)) {
+            if (!is_writable($path . "/")) {
+                $output->writeln("Error: Path is not writeable ! Exiting...");
+                return Command::FAILURE;
+            }
+
+            file_put_contents("$path/key.private", $private_key->encodePem());
+            file_put_contents("$path/key.pub", $public_key->encodePem());
+        }
 
         return Command::SUCCESS;
     }
